6464b6de43efd7a1deee477f5cb202785628e0d4
Evan Klitzke <evan@eklitzke.org>
2015-12-16 22:31:46 -0800

    fix error in cron

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd ~/code/hiphop-links &>/dev/null
 . env/bin/activate &>/dev/null
-hiphop-generate "~/.weechat/logs/irc.bitlbee.#hiphop.weechatlog"
+hiphop-generate ~/.weechat/logs/irc.bitlbee.#hiphop.weechatlog
 mv hiphop.html /var/smiley
 cp static/* /var/smiley
 popd &>/dev/null
