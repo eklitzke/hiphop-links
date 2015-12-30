9c3836871de9bc5f8cacbdd7ee6e37c08a292f12
Evan Klitzke <evan@uber.com>
2015-12-29 17:07:34 -0800

    fix again

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -3,6 +3,6 @@ pushd ~/code/hiphop-links &>/dev/null
 . env/bin/activate &>/dev/null
 hiphop-generate --title="hiphop links" --outfile="hiphop.html" ~/.weechat/logs/irc.bitlbee.#hiphop.weechatlog
 hiphop-generate --title="edm links" --outfile="edm.html" ~/.weechat/logs/irc.bitlbee.#edm.weechatlog
-mv hiphop.html edm.html /var/smiley
-cp static/* /var/smiley
+mv hiphop.html edm.html /var/www
+cp static/* /var/www
 popd &>/dev/null
