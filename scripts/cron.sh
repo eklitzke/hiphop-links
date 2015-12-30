9b0e4436fe5a6627fe1e81c43d32c72fab9aff4d
Evan Klitzke <evan@uber.com>
2015-12-29 17:08:03 -0800

    .

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pushd ~/code/hiphop-links &>/dev/null
+pushd ~/hiphop-links &>/dev/null
 . env/bin/activate &>/dev/null
 hiphop-generate --title="hiphop links" --outfile="hiphop.html" ~/.weechat/logs/irc.bitlbee.#hiphop.weechatlog
 hiphop-generate --title="edm links" --outfile="edm.html" ~/.weechat/logs/irc.bitlbee.#edm.weechatlog
